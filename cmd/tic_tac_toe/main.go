562979f028ce6fbeff93dbab48359f2c3aec9cc0
Adam Kubík <kubikadam2003@gmail.com>
2025-01-22 23:51:45 +0100

    build: make channels for the connections

--- cmd/tic_tac_toe/main.go
+++ cmd/tic_tac_toe/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	s := server.NewServer("127.0.0.1:8080")
 	fmt.Printf("starting server on %s\n", s.ListenAddr)
-	if err := server.ListenNewConn(s); err != nil {
+	if err := server.ListenAndPair(s); err != nil {
 		log.Printf("server failed: %v", err)
 	}
 }
--- internal/tic_tac_toe/models/server.go
+++ internal/tic_tac_toe/models/server.go
@@ -2,13 +2,10 @@ package models
 
 import (
 	"net"
-	"sync"
 )
 
 type Server struct {
 	ListenAddr string
-	Ln         net.Listener
-
-	Mu      sync.Mutex
-	Players [2]*Player
+	Listener   net.Listener
+	ConnsChan  chan net.Conn
 }
