c2bdc974531e5e3544e1a47434e914bd594bb7c8
Adam Kubík <kubikadam2003@gmail.com>
2025-01-22 20:54:53 +0100

    build: create server and start listen on it

--- cmd/tic_tac_toe/main.go
+++ cmd/tic_tac_toe/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	server "tic_tac_toe/internal/tic_tac_toe/handlers"
+)
 
+func main() {
+	s := server.NewServer("127.0.0.1:8080")
+	fmt.Printf("starting server on %s\n", s.ListenAddr)
+	if err := server.ListenNewConn(s); err != nil {
+		log.Printf("server failed: %v", err)
+	}
 }
