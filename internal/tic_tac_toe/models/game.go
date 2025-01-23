d3df72e73ed5f2dc3a3107908992d5baf497bd12
Tomáš Karol Hőger <tomaskarol.hoger@gmail.com>
2025-01-23 19:51:09 +0100

    Update Game and add GameResult

--- internal/tic_tac_toe/models/game.go
+++ internal/tic_tac_toe/models/game.go
@@ -4,7 +4,16 @@ type Game struct {
 	Player1       Player
 	Player2       Player
 	OnGoing       bool
-	CurrentPlayer Player
-	Board         [3][3]string
-	Winner        Player
+	CurrentPlayer *Player
+	WaitingPlayer *Player
+	Board         *[3][3]string
+	Winner        *Player
+	Loser         *Player
+}
+
+type GameResult struct {
+	Player1 Player
+	Player2 Player
+	Winner  *Player
+	Loser   *Player
 }
