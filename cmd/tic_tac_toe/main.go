3f78af3772aab5070a86917bb682a9c4f235329d
Adam Kubík <kubikadam2003@gmail.com>
2025-01-24 10:49:24 +0100

    build: add connection to google cloud based db

--- cmd/tic_tac_toe/main.go
+++ cmd/tic_tac_toe/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"fmt"
 	"log"
+	config "tic_tac_toe/db"
 	"tic_tac_toe/internal/tic_tac_toe/handlers"
 )
 
 func main() {
+	cfg := config.LoadConfig()
+	dB := config.InitDB(cfg)
+	defer dB.Close()
+
 	s := handlers.NewServer("127.0.0.1:8080")
 	fmt.Printf("starting server on %s\n", s.ListenAddr)
 	if err := handlers.ListenAndPair(s); err != nil {
